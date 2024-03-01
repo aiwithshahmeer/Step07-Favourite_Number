40e9999fda28c0237fdb9ed20d92b398b7d90c4d
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-03-01 22:35:32 +0500

    Add files via upload

---
+++ Favourite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const Favourite_Number = 7;
+const message = 'Here is your Favourite Number: ' + Favourite_Number;
+console.log(message);
---
+++ Favourite_Number.ts
@@ -0,0 +1,5 @@
+const Favourite_Number : number = 7;
+
+const message : string = 'Here is your Favourite Number: ' + Favourite_Number ;
+
+console.log(message) ;
